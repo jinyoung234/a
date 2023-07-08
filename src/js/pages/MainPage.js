5bc950acfa6c7c812ad42bacf91db9e6d38c4cbf
jinyoung <sm099@naver.com>
2023-07-09 00:03:32 +0900

    feat : MainPage 컴포넌트 마크업 완료

--- src/js/pages/MainPage.js
+++ src/js/pages/MainPage.js
@@ -1,5 +1,16 @@
 import Component from "../core/Component.js";
 
-class MainPage extends Component {}
+class MainPage extends Component {
+  template() {
+    return `
+        <div class="d-flex justify-center mt-5 w-100">
+            <div data-component="main-page" class="w-100">
+                 <header data-component="header"> </header>
+                 <main data-component="todoWrapper" class="mt-10 d-flex justify-center"></main>
+            <div/>
+        </di>
+    `;
+  }
+}
 
 export default MainPage;
