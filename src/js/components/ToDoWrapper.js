9b435d3539d20356cab217c7969f9095d4a45b07
jinyoung <sm099@naver.com>
2023-07-09 14:52:50 +0900

    feat : ToDoWrapper 내 ToDoItems와 ToDoCount 렌더링 하는 로직 추가

--- src/js/components/ToDoWrapper.js
+++ src/js/components/ToDoWrapper.js
@@ -1,26 +1,32 @@
 import Component from "../core/Component.js";
 import { $target } from "../util/dom.js";
+import ToDoCount from "./ToDoCount.js";
 import ToDoForm from "./ToDoForm.js";
+import ToDoItems from "./ToDoItems.js";
 import ToDoTitle from "./ToDoTitle.js";
 
 class ToDoWrapper extends Component {
   template() {
     return `
-        <div class="wrapper bg-white p-10">
+        <div id="todoWrapper" class="wrapper bg-white p-10">
             <div class="heading d-flex justify-between">
                 <div data-component="title"></div>
                 <div data-component="todoCount"></div>
             </div>
             <div data-component="todoForm"></div>
-            <ul id="menu-list" class="mt-3 pl-0"></ul>
+            <ul data-component="todoItems" id="menu-list" class="mt-3 pl-0"></ul>
         </div>
     `;
   }
   mounted() {
     const $title = $target(this.$target, '[data-component="title"]');
     const $todoForm = $target(this.$target, '[data-component="todoForm"]');
+    const $todoItems = $target(this.$target, '[data-component="todoItems"]');
+    const $todoCount = $target(this.$target, '[data-component="todoCount"]');
     new ToDoTitle($title, this.props);
     new ToDoForm($todoForm, this.props);
+    new ToDoItems($todoItems, this.props);
+    new ToDoCount($todoCount, this.props);
   }
 }
 
