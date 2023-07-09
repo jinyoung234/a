01ad6697c6af382bbc35874c1d4ddbb4f38db123
jinyoung <sm099@naver.com>
2023-07-09 14:50:47 +0900

    feat : ToDoCount 컴포넌트 마크업

---
+++ src/js/components/ToDoCount.js
@@ -0,0 +1,13 @@
+import Component from "../core/Component.js";
+
+class ToDoCount extends Component {
+  template() {
+    const { menu, curCategory } = this.props;
+    const menuCount = menu[curCategory].length;
+    return `
+            <p class="mr-2 mt-4 menu-count">총 ${menuCount} 개</p>
+        `;
+  }
+}
+
+export default ToDoCount;
